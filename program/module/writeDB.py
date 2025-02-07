0e5ad7bd62d6840a4668bc5fe6c12a69d71422b6
amafue <amandafuentes831@gmail.com>
2025-02-07 13:17:00 +0100

    initial configuration added to the back end and view folder created (frontend)

--- program/module/writeDB.py
+++ program/module/writeDB.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..','..')))
 import pyrebase
 
-class readDB:
+class writeDB:
     def __init__(self):
         config = {
             "apiKey": "AIzaSyBdeH9uelVdA54VNtFNtTiNXpWpI-qnVxw",
