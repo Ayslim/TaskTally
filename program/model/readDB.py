0175fb8365f24dd18036b8434339e46c0660a8c7
ddolkar61dechen <dechen.dolkar0008@stud.hkr.se>
2025-02-19 16:08:08 +0100

    Add files via upload

---
+++ program/model/readDB.py
@@ -0,0 +1,26 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+import pyrebase4
+
+class ReadDB:
+    def __init__(self):
+        config = {
+            "apiKey": "AIzaSyBdeH9uelVdA54VNtFNtTiNXpWpI-qnVxw",
+            "authDomain": "tasktally-d09dd.firebaseapp.com",
+            "databaseURL": "https://tasktally-d09dd-default-rtdb.europe-west1.firebasedatabase.app",
+            "projectId": "tasktally-d09dd",
+            "storageBucket": "tasktally-d09dd.firebasestorage.app",
+            "messagingSenderId": "892299139303",
+            "appId": "1:892299139303:web:36373d6a48011b39536a42",
+            "measurementId": "G-7GEWMKQSF2"
+            }
+        firebase = pyrebase.initialize_app(config)
+        self.database = firebase.database()
+        
+"""     def read_test(self):
+        task_list = self.database.child("tasks").get()
+        
+        for task in task_list:
+            print(task) """
+        
