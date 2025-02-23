6029d45c2973b2b4f0c8ad7efc856ea94fa03e8b
Ayslim <aysenurkarakus95@gmail.com>
2025-02-23 16:15:45 +0100

    Added some comments to explain what the code does, and improved function names

--- program/controller/task_list_controller.py
+++ program/controller/task_list_controller.py
@@ -2,13 +2,21 @@ import firebase_admin
 from firebase_admin import db, credentials
 
 class TaskListController:
+
+    #Setting up the connection to the database:
     def __init__(self):
         cred = credentials.Certificate("credentials.json")
         firebase_admin.initialize_app(cred, { "databaseURL": "https://tasktally-d09dd-default-rtdb.europe-west1.firebasedatabase.app"})
         self.ref = db.reference("/tasks")
 
+    #Gets tasks from the database:
+    # [
+    # {"Task 1": {"completed": True, "score": 5}},
+    # {"Task 2": {"completed": False, "score": 10}}
+    # ]
     def fetch_from_firebase(self):
         return self.ref.get()
     
-    def set_in_firebase(self, task_description, completed):
+    #Changes the completed status of a specific task:
+    def set_completed_status_on_task(self, task_description, completed):
         return self.ref.child(task_description).update({"completed": completed})
--- program/view/task_page.py
+++ program/view/task_page.py
@@ -10,11 +10,13 @@ class TaskPage(QtWidgets.QWidget, Ui_Form):
         # Don't update this variable, just use it to look up scores
         self.data_from_db = controller.fetch_from_firebase()
 
+        #task_data is the value for the task key.
         for task_description, task_data in self.data_from_db.items():
-            checked = task_data["completed"]
-            label = f"({task_data["score"]}) {task_description}"
+            task_completed = task_data["completed"]
+            score = task_data["score"]
+            label = f"({score}) {task_description}"
             item = QtWidgets.QListWidgetItem(label)
-            item.setCheckState(QtCore.Qt.Unchecked if not checked else QtCore.Qt.Checked)
+            item.setCheckState(QtCore.Qt.Checked if task_completed else QtCore.Qt.Unchecked)
             self.taskListView.addItem(item)
 
         self.resetAllTasksBtn.clicked.connect(self.resetAllTasks)
@@ -23,29 +25,36 @@ class TaskPage(QtWidgets.QWidget, Ui_Form):
     
     # Updates each item in the list, which causes taskUpdated() to be run, which updates the total score and the completed status in the db
     def resetAllTasks(self):
-        for item in self.get_list_items():
+        for item in self.get_list_of_QListWidgetItems():
             item.setCheckState(QtCore.Qt.Unchecked)
 
+    # The task wask checked or unchecked in the UI. Let's update the completed status in the database, and update the total score label
     def taskUpdated(self, item):
-        task_description, checked = self.getTextAndChecked(item)
-        self.controller.set_in_firebase(task_description, checked)
+        task_description, checked = self.getTaskDescriptionAndCheckedStatusFromQListWidgetItem(item)
+        # Setting the checked status in the database using the controller
+        self.controller.set_completed_status_on_task(task_description, checked)
         self.updateTotalScore()
 
     def updateTotalScore(self):
         score = 0
-        for item in self.get_list_items():
-            task_description, checked = self.getTextAndChecked(item)
+        for item in self.get_list_of_QListWidgetItems():
+            task_description, checked = self.getTaskDescriptionAndCheckedStatusFromQListWidgetItem(item)
             if(checked):
                 score += self.data_from_db[task_description]["score"]
         self.totalScore.setText(str(score))
 
-    def getTextAndChecked(self, item):
+    # Getting the task description and checked status from the QListWidget (not the db!) 
+    def getTaskDescriptionAndCheckedStatusFromQListWidgetItem(self, item):
         # Split on the space between points and task description. Only do one split, and get the second element in the result (task description)
+        # (20)> <Clean your room
         task_description = item.text().split(" ", 1)[1]
         checked = True if item.checkState() == QtCore.Qt.CheckState.Checked else False
         return task_description, checked
     
-    def get_list_items(self):
+    def get_list_of_QListWidgetItems(self):
+        # There is no easy way to get all the items from the QListWidget
+        # This is a list comprehension that gets item 0, item 1, item 2, etc.
+        # This returns a list of QListWidgetItem
         return [self.taskListView.item(x) for x in range(self.taskListView.count())]
         
        
